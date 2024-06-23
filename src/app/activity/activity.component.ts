d91d58a9245c51b781daeaf11582d0b1f203d972
VeselinTodorov2000 <veselin.todorov@dreamix.eu>
2024-06-23 19:38:07 +0300

    add form and do reworks

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {Activity, StepNumber} from "../models/activity";
 import {ActivityStatus} from "../models/activity-status";
 import {ActivityType} from "../models/activity-type";
+import {Homeowner} from "../models/homeowner";
+import {Offer} from "../models/offer";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-activity',
@@ -9,6 +12,7 @@ import {ActivityType} from "../models/activity-type";
   styleUrls: ['./activity.component.css']
 })
 export class ActivityComponent implements OnInit {
+  currentUser!: Homeowner;
   activity: Activity = {
     name: 'Примерна Дейност',
     responsible: 'Иван Иванов',
@@ -23,22 +27,41 @@ export class ActivityComponent implements OnInit {
     ]
   };
 
+  offers: Offer[] = [
+    {
+      executor: "Людмил Събев",
+      description: "Фирма Болгарстрой",
+      price: 116.28
+    }
+  ];
+
   ngOnInit(): void {
+    this.currentUser = JSON.parse(sessionStorage.getItem('currentUser') || '');
   }
+
   activityStatusNames = ActivityStatus;
   activityTypeNames = ActivityType;
 
   currentStepIndex: number = 0;
 
   stepNames = StepNumber;
+  offerForm = new FormGroup({
+    activityDescription: new FormControl('', Validators.required),
+    price: new FormControl('', [Validators.required, Validators.min(0)]),
+  });
 
   getStepName(step: StepNumber): string {
     switch (step) {
-      case StepNumber.POLL: return 'POLL';
-      case StepNumber.OFFER_COLLECTION: return 'OFFER_COLLECTION';
-      case StepNumber.OFFER_SELECTION: return 'OFFER_SELECTION';
-      case StepNumber.FUNDRAISING: return 'FUNDRAISING';
-      default: return 'Unknown Step';
+      case StepNumber.POLL:
+        return 'POLL';
+      case StepNumber.OFFER_COLLECTION:
+        return 'OFFER_COLLECTION';
+      case StepNumber.OFFER_SELECTION:
+        return 'OFFER_SELECTION';
+      case StepNumber.FUNDRAISING:
+        return 'FUNDRAISING';
+      default:
+        return 'Unknown Step';
     }
   }
 
@@ -55,7 +78,12 @@ export class ActivityComponent implements OnInit {
   }
 
   onAddOffer(): void {
-    // Implement the logic to add an offer
+    this.offers.push({
+      executor: this.currentUser.homeOwnerName,
+      description: this.offerForm.get('activityDescription')?.value!,
+      price: parseFloat(this.offerForm.get('price')?.value!)
+    });
+    this.currentStepIndex++;
   }
 
   onFundraise(): void {
